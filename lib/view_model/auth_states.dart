02466524fe9a280a98024d2c0a020868a5907cd2
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-03-15 15:44:56 +0200

    auth states

---
+++ lib/view_model/auth_states.dart
@@ -0,0 +1,23 @@
+part of 'auth_cubit.dart';
+
+abstract class AuthStates {}
+
+class AuthInitial extends AuthStates {}
+
+class SignInLoadingState extends AuthStates {}
+
+class SignInSuccessState extends AuthStates {}
+
+class SignInErrorState extends AuthStates {}
+
+class SignUpLoadingState extends AuthStates {}
+
+class SignUpSuccessState extends AuthStates {}
+
+class SignUpErrorState extends AuthStates {}
+
+class GoogleSignInLoadingState extends AuthStates {}
+
+class GoogleSignInSuccessState extends AuthStates {}
+
+class GoogleSignInErrorState extends AuthStates {}
