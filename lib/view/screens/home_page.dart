7f735a0415c024a3e2e69e750c2e5c805fdc1bc6
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-03-15 18:50:53 +0200

    sign out button added

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -1,4 +1,6 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:login_signup_pages/view/widgets/primary_button.dart';
 import 'package:login_signup_pages/view_model/auth_cubit.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,16 +10,33 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var cubit = AuthCubit.get(context);
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Welcome!! ${cubit.name}",
-          textAlign: TextAlign.center,
-          style: const TextStyle(
-            color: Colors.green,
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                "Welcome!! ${cubit.name}",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.green,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20),
+            child: PrimaryButton(
+              text: "Sign Out",
+              onPressed: () {
+                log(cubit.state.toString());
+                cubit.signOut(context);
+                log(cubit.state.toString());
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
