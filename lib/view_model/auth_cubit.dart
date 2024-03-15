4c0b8151fc5c84edf17d93285a2c75c784d029ef
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-03-15 15:48:34 +0200

    signInWithEmailAndPassword, registerWithEmailAndPassword and signInWithGoogle methods created

---
+++ lib/view_model/auth_cubit.dart
@@ -0,0 +1,94 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login_signup_pages/view/screens/home_page.dart';
+import 'package:login_signup_pages/view_model/google.dart';
+
+part 'auth_states.dart';
+
+class AuthCubit extends Cubit<AuthStates> {
+  AuthCubit() : super(AuthInitial());
+  static AuthCubit get(context) => BlocProvider.of(context);
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  late String name, email, password;
+
+  void signInWithEmailAndPassword(BuildContext context) {
+    emit(SignInLoadingState());
+
+    try {
+      _auth
+          .signInWithEmailAndPassword(email: email, password: password)
+          .then((value) async {
+        emit(SignInSuccessState());
+
+        name = email.split("@")[0];
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomePage(),
+          ),
+        );
+      }).catchError((error) {
+        emit(SignInErrorState());
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(error.toString())),
+        );
+      });
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  void registerWithEmailAndPassword(BuildContext context) {
+    emit(SignUpLoadingState());
+
+    try {
+      _auth
+          .createUserWithEmailAndPassword(email: email, password: password)
+          .then((user) async {
+        emit(SignUpSuccessState());
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomePage(),
+          ),
+        );
+      }).catchError((error) {
+        emit(SignUpErrorState());
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(error.toString())),
+        );
+      });
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  void signInWithGoogle(BuildContext context) {
+    emit(GoogleSignInLoadingState());
+
+    try {
+      signInWithGoogleCredentials().then((value) async {
+        emit(GoogleSignInSuccessState());
+        name = value.user!.displayName!;
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomePage(),
+          ),
+        );
+      }).catchError((error) {
+        emit(GoogleSignInErrorState());
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(error.toString())),
+        );
+      });
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+}
